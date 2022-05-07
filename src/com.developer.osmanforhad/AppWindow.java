4b49b6235f823e24e9fc82e8c2f9fa80e53e9798
osman forhad <osmanforhad@pop-os.localdomain>
2022-05-07 16:42:42 +0600

    working with Thread programming in Digital Clock method

--- src/com.developer.osmanforhad/AppWindow.java
+++ src/com.developer.osmanforhad/AppWindow.java
@@ -1,64 +1,69 @@
 import java.awt.Font;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.Timer;
-
 import java.awt.GridLayout;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class AppWindow extends JFrame{
+public class AppWindow extends JFrame {
 
-    //__Define variables__//
-    private JLabel heading;
-    private JLabel clock;
-    private Font font = new Font("", Font.BOLD, 35);
+  //__Define variables__//
+  private JLabel heading;
+  private JLabel clock;
+  private Font font = new Font("", Font.BOLD, 35);
 
-    //__Constructor Method__//
-    AppWindow(){
-        super.setTitle("Digital Clock by osman forhad");
-        super.setSize(620, 620);
-        setLocation(300, 50);
-        //Calling the User Interface Method
-        this.createGUI();
-          //Calling the digitalClock Method
-          digitalClock();
-        super.setVisible(true);
-    }
+  //__Constructor Method__//
+  AppWindow() {
+    super.setTitle("Digital Clock by osman forhad");
+    super.setSize(400, 400);
+    setLocation(300, 50);
+    //Calling the User Interface Method
+    this.createGUI();
+    //Calling the digitalClock Method
+    digitalClock();
+    super.setVisible(true);
+  }
 
-    //__All User interface method__//
-    public void createGUI(){
-        //create heading object
-        heading = new JLabel("Digital Clock");
-        //create clock object
-        clock = new JLabel("Clock");
+  //__All User interface method__//
+  public void createGUI() {
+    //create heading object
+    heading = new JLabel("Digital Clock");
+    //create clock object
+    clock = new JLabel("Clock");
 
-        //Font setup
-        heading.setFont(font);
-        clock.setFont(font);
+    //Font setup
+    heading.setFont(font);
+    clock.setFont(font);
 
-        //put heding and clock inside the application frame by using grid layout
-        this.setLayout(new GridLayout(2, 1));
-        this.add(heading);
-        this.add(clock);
+    //put heding and clock inside the application frame by using grid layout
+    this.setLayout(new GridLayout(2, 1));
+    this.add(heading);
+    this.add(clock);
+  }
 
-    }
+  //__Digital Clock Method Using Thread Programming__//
+  public void digitalClock() {
+    Thread thread = new Thread() {
+      public void run() {
+        try {
+          while (true) {
+            //Time Formating by SimpleDateFormat class
+            Date date = new Date();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+              "hh : mm : ss a"
+            );
+            String dateTime = simpleDateFormat.format(date);
+            //set date time inside the clock label
+            clock.setText(dateTime);
 
-    //__Digital Clock Method__//
-    public void digitalClock(){
-        Timer timer = new Timer(1000, new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent e) {
-                //Time Formating by SimpleDateFormat class
-                Date date = new Date();
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh : mm : ss a");
-                String dateTime = simpleDateFormat.format(date);
-                //set date time inside the clock label
-                clock.setText(dateTime);
-            }
-        });
-        timer.start();
-    }
-
+            //Thread.sleep(1000);
+            Thread.currentThread().sleep(1000);
+          }
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    };
+    thread.start();
+  }
 }
--- src/com.developer.osmanforhad/Main.java
+++ src/com.developer.osmanforhad/Main.java
@@ -1,7 +1,8 @@
-public class Main{
-    public static void main(String args[]){
-      System.out.println("Testing...");
-      //__Create Aplication Frame object__//
-      AppWindow AplicationFraming = new AppWindow();
-    }
+public class Main {
+
+  public static void main(String args[]) {
+    System.out.println("Testing...");
+    //__Create Aplication Frame object__//
+    AppWindow AplicationFraming = new AppWindow();
+  }
 }
