7071243077701f56d33612df95f5f1464b4c8b85
osman forhad <osmanforhad@pop-os.localdomain>
2022-05-07 12:54:11 +0600

    setup application window

---
+++ src/com.developer.osmanforhad/AppWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class AppWindow extends JFrame{
+    //__Constructor Method__//
+    AppWindow(){
+        super.setTitle("Digital Clock by osman forhad");
+        super.setSize(400, 400);
+        setLocation(300, 50);
+        super.setVisible(true);
+    }
+
+}
